c4e5df5dcf54dbb90dba96413aa30c100c78e424
Pratyush Pradhan <pradhan37202@gmail.com>
2021-03-06 14:38:43 +0530

    Add scores controller and setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "scores#index"
+
+  get 'scores/index'
+  get 'scores/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
