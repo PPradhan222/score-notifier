29890ee804cd9bdb60500fff5b6cc9a0333c9dce
Pratyush Pradhan <pradhan37202@gmail.com>
2021-03-09 12:58:41 +0530

    Add dependent destroy to relevant models

--- app/models/inning.rb
+++ app/models/inning.rb
@@ -1,10 +1,10 @@
 class Inning < ApplicationRecord
   belongs_to :match
-  has_many :batting_player_innings
-  has_many :bowling_player_innings
+  has_many :batting_player_innings, dependent: :destroy
+  has_many :bowling_player_innings, dependent: :destroy
   has_many :batsmen, through: :batting_player_innings, source: :player
   has_many :bowlers, through: :bowling_player_innings, source: :player
-  has_many :balls
+  has_many :balls, dependent: :destroy
 
   delegate :teams, to: :match
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,4 @@
 class Team < ApplicationRecord
-  has_many :match_teams
+  has_many :match_teams, dependent: :destroy
   has_many :matches, through: :match_teams
 end
