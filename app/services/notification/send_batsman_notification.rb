c7ac1cab1649a9483c1113076f8eba843d920636
Pratyush Pradhan <pradhan37202@gmail.com>
2021-04-19 15:49:43 +0530

    Add basic push message service

--- app/services/notification/send_batsman_notification.rb
+++ app/services/notification/send_batsman_notification.rb
@@ -1,10 +1,11 @@
 module Notification
   class SendBatsmanNotification
-    attr_reader :match_key, :batsmen_scorecard_data
+    attr_reader :match_key, :batsmen_scorecard_data, :batsmen_score_hash
 
     def initialize(match_key)
       @match_key = match_key
       @batsmen_scorecard_data = []
+      @batsmen_score_hash = {}
     end
 
     def call
@@ -27,6 +28,8 @@ module Notification
           batsman[:sixes] = batsman_data["sixes"]
           batsman[:fours] = batsman_data["fours"]
           batsman[:balls_faced] = batsman_data["balls_faced"]
+          batsman[:name] = batsman_data["profile_url"]&.split("/")&.dig(-1)&.titleize
+          batsmen_score_hash[batsman[:batsman_match_id]] = "#{batsman[:name]} R: #{batsman[:runs_scored] || 0}, B: #{batsman[:balls_faced] || 0}, 6s: #{batsman[:sixes] || 0}, 4s: #{batsman[:fours] || 0}, SR: #{batsman[:strike_rate] || 0.0}"
           batsmen_scorecard_data << batsman
         end
       end
@@ -38,7 +41,36 @@ module Notification
         batsman_notifications += BatsmanScoreNotifier.where(batsman_match_id: batsman[:batsman_match_id])
         .where("balls_faced <= ? OR runs_scored <= ? OR sixes <= ? OR fours <= ?", batsman[:balls_faced], batsman[:runs_scored], batsman[:sixes], batsman[:fours])
       end
-      SendPushNotificationWorker.perform_async(batsman_notifications) unless batsman_notifications.blank?
+      # SendPushNotificationWorker.perform_async(batsman_notifications) unless batsman_notifications.blank?
+      send_push_notifications(batsman_notifications) unless batsman_notifications.blank?
+    end
+
+    def send_push_notifications(batsman_notifications)
+      batsman_notifications.group_by(&:user_id).each do |user_id, notifications|
+        user = User.find_by(id: user_id)
+        message = create_message(notifications.pluck(:batsman_match_id))
+        # binding.pry
+        web_push(message, user&.endpoint, user&.p256dh, user&.auth)
+      end
+    end
+
+    def web_push(message, user_endpoint, user_p256dh, user_auth)
+      Webpush.payload_send(
+        message: message,
+        endpoint: user_endpoint,
+        p256dh: user_p256dh,
+        auth: user_auth,
+        vapid: {
+          public_key: ENV['VAPID_PUBLIC_KEY'],
+          private_key: ENV['VAPID_PRIVATE_KEY']
+        }
+      )
+    end
+
+    def create_message(batsman_match_ids)
+      message = ""
+      batsman_match_ids.map {|id| message += "#{batsmen_score_hash[id]}/n"}
+      message
     end
   end
 end
--- app/workers/send_push_notification_worker.rb
+++
@@ -1,8 +0,0 @@
-class SendPushNotificationWorker
-  include Sidekiq::Worker
-  sidekiq_options retry: false, queue: 'notification'
-
-  def perform(notifications)
-    # notifications.group_by(&:user_id)
-  end
-end
