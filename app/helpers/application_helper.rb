c6757cfaf197f4c075f4b1ad72c9b900763fc639
Pratyush Pradhan <pradhan37202@gmail.com>
2021-04-25 12:19:28 +0530

    Creata match cards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  # def abbreviate(phrase)
+  #   if(phrase.split(" ").length )
+  #   phrase.gsub('-', ' ')
+  #         .scan(/(\A\w|(?<=\s)\w)/)
+  #         .flatten
+  #         .join.upcase
+  #    end     
+  # end
+
+  def match_number(match)
+    match.split(",").drop(1).join(", ")
+  end
+
+  def match_name(match)
+    match.split(",").first
+  end
 end
